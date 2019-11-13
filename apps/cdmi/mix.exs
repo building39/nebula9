01ddd2ede57ff224eacfcdea6bb08625e9dbbcfc
mmartin <mmartin4242@gmail.com>
2019-11-13 14:46:28 -0500

    Enable coveralls

--- apps/cdmi/mix.exs
+++ apps/cdmi/mix.exs
@@ -14,6 +14,8 @@ defmodule Cdmi.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       deps: deps()
     ]
   end
