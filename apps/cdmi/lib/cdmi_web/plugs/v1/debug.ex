db2a49a0056ce19cc185076017e38411496e6f09
mmartin <mmartin4242@gmail.com>
2019-11-20 16:58:32 -0500

    Remove unneeded imports

--- apps/cdmi/lib/cdmi_web/plugs/v1/debug.ex
+++ apps/cdmi/lib/cdmi_web/plugs/v1/debug.ex
@@ -2,11 +2,9 @@ defmodule CdmiWeb.Plugs.V1.Debug do
   @moduledoc """
   Check the X-CDMI-Specification-Version request header.
   """
-
-  import Plug.Conn
-  import Phoenix.Controller
   require Logger
 
+  @spec init(map()) :: map()
   def init(opts) do
     Logger.debug("Plug Debug init")
     opts
@@ -15,6 +13,7 @@ defmodule CdmiWeb.Plugs.V1.Debug do
   @doc """
   Check the X-CDMI-Specification-Version header against the versions in config.
   """
+  @spec call(Plug.Conn.t, map()) :: Plug.Conn.t()
   def call(conn, _opts) do
     Logger.debug("Debug plug. CONN: #{inspect conn, pretty: true}")
     conn
